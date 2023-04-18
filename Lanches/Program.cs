ef99d013794196536b701048bfe3ce544becdfbf
VitorEAFarias <vphda7x@gmail.com>
2023-04-18 17:20:01 -0300

    Migrations Categorias e Lanches

--- Lanches/Program.cs
+++ Lanches/Program.cs
@@ -1,7 +1,13 @@
+using Lanches.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
