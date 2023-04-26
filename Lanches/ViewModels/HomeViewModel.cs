cb1781e2212cee3d911ae78908ce1dc781cf8ddc
VitorEAFarias <vphda7x@gmail.com>
2023-04-26 17:54:29 -0300

    Design e Contatos

--- Lanches/ViewModels/HomeViewModel.cs
+++ Lanches/ViewModels/HomeViewModel.cs
@@ -1,6 +1,9 @@
-﻿namespace Lanches.ViewModels
+﻿using Lanches.Models;
+
+namespace Lanches.ViewModels
 {
     public class HomeViewModel
     {
+        public IEnumerable<Lanche> LanchesPreferidos { get; set; }
     }
 }
